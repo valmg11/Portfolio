221c786c90830daf0c11793ba0c247719157f085
valmg11 <tictactina05@gmail.com>
2026-04-25 20:19:14 -0400

    edit navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,18 +4,18 @@ function Navbar() {
             <div className="navBar">
                 <ul>
                     <li className="home">
-                        <a className="nav" href="#intro">Val</a>
+                        <a className="navLinks" href="#intro">Val</a>
                     </li>
                 </ul>
                 <ul>
                      <li>
-                        <a className="nav" href="#about">About</a>
+                        <a className="navLinks" href="#about">About</a>
                     </li>
                     <li>
-                        <a className="nav" href="#projects">Projects</a>
+                        <a className="navLinks" href="#projects">Projects</a>
                     </li>
                     <li>
-                        <a className="nav" href="#contact">Contact</a>
+                        <a className="navLinks" href="#contact">Contact</a>
                     </li>
                 </ul>
             </div>
