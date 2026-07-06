d2a0a1cc30473638faa9a9e5f7109fc86423bc9b
valmg11 <tictactina05@gmail.com>
2026-07-05 21:56:28 -0400

    edit contact and projects

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,9 @@
-const contacts = ["LinkedIn", "Handshake", "GitHub", "Instagram"]
+import LinkedIn from '../assets/linkedin.png';
+import Handshake from '../assets/handshake.png';
+import Github from '../assets/github.png';
+
+
+// const contacts = ["LinkedIn", "Handshake", "GitHub", "Instagram"]
 
 function Contact(props) {
     return (
@@ -9,12 +14,9 @@ function Contact(props) {
         </div>
         <div className="contactDiv">
             <div className="buttonDiv">
-                <a href="https://www.linkedin.com/in/valentina-gandolfo-126046306/" className="contactButtons">{contacts[0]}</a> <br></br>
-                <a href="https://ucf.joinhandshake.com/profiles/5gen4z" className="contactButtons">{contacts[1]}</a>
-            </div>
-            <div className="buttonDiv">
-                <a href="https://github.com/valmg11" className="contactButtons">{contacts[2]}</a> <br></br>
-                <a href="https://www.instagram.com/valmgandolfo/" className="contactButtons">{contacts[3]}</a>
+                <a href="https://ucf.joinhandshake.com/profiles/5gen4z" className="contactButtons"><img src={Handshake} alt="Handshakes Logo" style={{ width: '50px' }}/></a>
+                <a href="https://www.linkedin.com/in/valentina-gandolfo-126046306/" className="contactButtons"><img src={LinkedIn} alt="LinkedIn Logo" style={{ width: '50px' }}/></a> <br></br>
+                <a href="https://github.com/valmg11" className="contactButtons"><img src={Github} alt="Github Logo" style={{ width: '50px' }}/></a> <br></br>
             </div>
         </div>
         </section>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,8 @@ import project1 from '../assets/CMS.jpg'
 import project2 from '../assets/chatPage.jpg'
 import project3 from '../assets/todo.jpg'
 import project4 from '../assets/exerciseApp.jpg'
+import thrifted from '../assets/thrifted.png'
+
 
 
 const projects = [
@@ -21,6 +23,11 @@ const projects = [
         desc: "This is a functional to-do list made with React Native Expo. Users are able to add new tasks, as well as mark these tasks as completed or incomplete."
     },
     {
+        name: "Thrifted",
+        date: "April 2026",
+        desc: "Using User-Interface skills, a team and I created a functional prototype for a thrifting app using Figma. Thrifted is intended to help users more easily locate thrift stores for whatever their needs may be."
+    },
+     {
         name: "Exercise App",
         date: "April 2026",
         desc: "This exercise app was create with React and tracks 6 different exercises. Users can choose to train with a duration exercise, repetition exercise, or a strength exercise."
@@ -35,14 +42,6 @@ function Projects(props) {
                 <h2>{props.data[2]}</h2>
             </div>
             <div className="projectsDiv">
-                <div className="project1">
-                    <p className='projectTitle1'>{projects[0].name}</p>
-                    <p className="projectDate">{projects[0].date}</p>
-                    <div className="project1Flex">
-                        <img className="project1Img" src={project1} alt="Game Review CMS"/>
-                        <p>{projects[0].desc}</p>
-                    </div>
-                </div>
                 <div className="project2">
                     <p className='projectTitle'>{projects[1].name}</p>
                     <p className="projectDate">{projects[1].date}</p>
@@ -50,17 +49,17 @@ function Projects(props) {
                         <p>{projects[1].desc}</p>
                         <img className="project2Img" src={project2} alt="fAInman"/>
                     </div>
-                    <a href="https://github.com/jm19pa/Feynman-AI" className='projectDetails'>view details</a>
+                    <a href="https://github.com/jm19pa/Feynman-AI" target="_blank" rel="noopener noreferrer" className='projectDetails'>view project</a>
                 </div>
-                
-                <div className="project3">
-                    <p className='projectTitle'>{projects[2].name}</p>
-                    <p className='projectDate'>{projects[2].date}</p>
-                    <div className="project3Flex">
-                        <img className="project3Img" src={project3} alt="To-Do List"/>
-                        <p>{projects[2].desc}</p>
+
+                <div className="project1">
+                    <p className='projectTitle1'>{projects[0].name}</p>
+                    <p className="projectDate">{projects[0].date}</p>
+                    <div className="project1Flex">
+                        <img className="project1Img" src={project1} alt="Game Review CMS"/>
+                        <p>{projects[0].desc}</p>
                     </div>
-                    <a href="https://valmg11.github.io/expo-todo-test/" className='projectDetails'>view details</a>
+                    <a href="https://students.gaim.ucf.edu/~va717889/dig3134c/assignments/assignment04/login.php" target="_blank" rel="noopener noreferrer" className='projectDetails'>view project</a>
                 </div>
 
                 <div className="project4">
@@ -68,9 +67,29 @@ function Projects(props) {
                     <p className='projectDate'>{projects[3].date}</p>
                     <div className="project4Flex">
                         <p>{projects[3].desc}</p>
+                        <img className="thrifted" src={thrifted} alt="Exercise App"/>
+                    </div>
+                    <a href="https://www.figma.com/proto/1A4ztdSGytBh8o8G66g4ik/Thrifted---Lab-9?node-id=2080-295&p=f&t=TyBEMzneUzGo7DtN-0&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=2080%3A295" target="_blank" rel="noopener noreferrer" className='projectDetails'>view project</a>
+                </div>
+            
+                <div className="project3">
+                    <p className='projectTitle'>{projects[2].name}</p>
+                    <p className='projectDate'>{projects[2].date}</p>
+                    <div className="project3Flex">
+                        <img className="project3Img" src={project3} alt="To-Do List"/>
+                        <p>{projects[2].desc}</p>
+                    </div>
+                    <a href="https://valmg11.github.io/expo-todo-test/" target="_blank" rel="noopener noreferrer" className='projectDetails'>view project</a>
+                </div>
+
+                <div className="project3">
+                    <p className='projectTitle'>{projects[4].name}</p>
+                    <p className='projectDate'>{projects[4].date}</p>
+                    <div className="project3Flex">
+                        <p>{projects[4].desc}</p>
                         <img className="project4Img" src={project4} alt="Exercise App"/>
                     </div>
-                    <a href="https://valmg11.github.io/Updated-Exercise-App/" className='projectDetails'>view details</a>
+                    <a href="https://valmg11.github.io/Updated-Exercise-App/" target="_blank" rel="noopener noreferrer" className='projectDetails'>view project</a>
                 </div>
             </div>
         </section>
